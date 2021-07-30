b8765d0b876f51c832b5220942cea4cc5bb703ae
DenSP <DenSP@DENIS>
2021-07-30 08:42:37 +0300

    132

--- EtPDF/Program.cs
+++ EtPDF/Program.cs
@@ -20,38 +20,65 @@ namespace EtPDF
 
 
 
-        public static bool PrintPDF(string printer, int wi, int he, string filename, int copies)
+        public static bool PrintPDF(string printer, string paperName, int wi, int he, string filename, int copies)
         {
-            
+
             try
             {
-                printer = Console.ReadLine();
+                //printer = Console.ReadLine();
 
-                    using (var document = PdfiumViewer.PdfDocument.Load(filename))
+                using (var document = PdfiumViewer.PdfDocument.Load(filename))
+                {
+                    using (var printDocument = document.CreatePrintDocument(PdfPrintMode.CutMargin))
                     {
-                        using (var printDocument = document.CreatePrintDocument(PdfPrintMode.CutMargin))
-                        {
 
                         try
                         {
-                            
+
 
                             printDocument.PrinterSettings.PrinterName = printer;
                             printDocument.OriginAtMargins = true;
-                           
 
-                            PaperSize paperSize = new PaperSize("Test", (int)(wi/0.254), (int)(he/0.254));
-                            //paperSize.RawKind = (int)PaperKind.Custom;
-                            printDocument.PrinterSettings.DefaultPageSettings.PaperSize = paperSize;
-                            printDocument.DefaultPageSettings.PaperSize = paperSize;
-                            printDocument.PrinterSettings.DefaultPageSettings.Margins = new Margins(0, 0, 0, 0);
-                            printDocument.DefaultPageSettings.Margins = new Margins(0, 0, 0, 0);
+                            var printerSettings = new PrinterSettings
+                            {
+                                PrinterName = printer,
+                                Copies = (short)copies,
+                            };
+
+                            var pageSettings = new PageSettings(printerSettings);
+
+                            foreach (PaperSize paperSize in printerSettings.PaperSizes)
+                            {
+                                
+                                if (paperSize.PaperName == paperName)
+                                {
+                                    Console.WriteLine("+++");
+                                    printDocument.PrinterSettings.DefaultPageSettings.PaperSize = paperSize;
+                                    printDocument.PrinterSettings.DefaultPageSettings.Landscape = false; ////??? 
+                                    
+
+                                    //PaperSize paperSize = new PaperSize("Test", (int)(wi / 0.254), (int)(he / 0.254));
+                                    printDocument.DefaultPageSettings.PaperSize.RawKind = (int)PaperKind.Custom;
+                                    
+                                    printDocument.PrinterSettings.DefaultPageSettings.Margins = new Margins(50, 50, 50, 50);
+                                    printDocument.DefaultPageSettings.Margins = new Margins(50, 50, 50, 50);
+                                    break;
+                                }
+                            }
+
+                            
+
+
 
 
 
                             printDocument.PrintController = new StandardPrintController();
                             Console.WriteLine(">>" + printDocument.PrinterSettings.DefaultPageSettings);
-                            //printDocument.Print();
+
+
+                            printDocument.Print();
+
+
                         }
                         catch (Exception ex)
                         {
@@ -59,13 +86,13 @@ namespace EtPDF
                         }
 
                     }
-                    }
-                    return true;
                 }
-                catch
-                {
-                    return false;
-                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
         }
 
 
@@ -82,58 +109,85 @@ namespace EtPDF
         static PdfContentByte cb;
         const int wmargin = 1;
 
+        static string PDFName = Path.GetTempPath() + "my.pdf";
+        static string barcode = "";
+        static string title = "";
+        static string name = "";
+        static string modul = "";
+        static string color = "";
+        static string up = "";
+        static string ups = "";
+        static string typeup = "";
+        static bool furn = false;
+        static string packer = "";
+        static int upcol = 1;
+        static string trademark = "";
+        static string garant = "";
+        static string regnum = "";
+        static string sertifikatdata = "";
+        static string data = "";
+        static int print = 0;
+        static Dictionary<string, object> d;
+
+
         static int mm(double t)
-        { 
-            return Convert.ToInt32(t * 72 / 25.4);
+        {
+            return (int)Math.Round(t * 72 / 25.4);
         }
+       
         static void Main(string[] args)
         {
-
             consPrinterName();
 
-            string PDFName = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\my.pdf"; ;
-            
             if (args.Length > 0)
             {
-                string json = String.Join(" ", args);
+                string json = String.Join(" ", args).Replace(@"\'", "\"");
                 Console.WriteLine(json);
-                var values = JsonConvert.DeserializeObject<Dictionary<string, object>>(json);
-                if (values.ContainsKey("pdfname"))
-                    PDFName = values["pdfname"].ToString();
+
+                d = JsonConvert.DeserializeObject<Dictionary<string, object>>(json);
+                if (d.ContainsKey("pdfname")) PDFName = d["pdfname"].ToString();
+                if (d.ContainsKey("barcode")) barcode = d["barcode"].ToString();
+                if (d.ContainsKey("title")) title = d["title"].ToString();
+                if (d.ContainsKey("name")) name = d["name"].ToString();
+                if (d.ContainsKey("modul")) modul = d["modul"].ToString();               
+                if (d.ContainsKey("color")) color = d["color"].ToString();               
+                if (d.ContainsKey("up")) up = d["up"].ToString();               
+                if (d.ContainsKey("ups")) ups = d["ups"].ToString();               
+                if (d.ContainsKey("typeup")) typeup = d["typeup"].ToString();               
+                if (d.ContainsKey("furn")) furn = (bool)d["furn"];
+                if (d.ContainsKey("packer")) packer = d["packer"].ToString();
+                if (d.ContainsKey("upcol")) upcol = (int)d["upcol"];
+                if (d.ContainsKey("trademark")) trademark = d["trademark"].ToString();
+                if (d.ContainsKey("garant")) garant = d["garant"].ToString();
+                if (d.ContainsKey("regnum")) regnum = d["regnum"].ToString();
+                if (d.ContainsKey("sertifikatdata")) sertifikatdata = d["sertifikatdata"].ToString();
+                if (d.ContainsKey("data")) data = d["data"].ToString();
+                if (d.ContainsKey("print")) print = (int)d["print"];
             }
 
 
-           
+            Console.WriteLine(PDFName);
+            //Console.ReadKey();
 
-
-            int wi = Convert.ToInt32(150 / 25.4 * 72);
-            int he = Convert.ToInt32(28 / 25.4 * 72);
-
-
-            //------------------------------------------------------------------------------------------------------------
+            float wi = (float)(210 / 25.4 * 72);
+            float he = (float)(297 / 25.4 * 72);
+            
+            //-------------------------------------------------------------------------------------------------------------
             string t = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Fonts), "times.TTF");
             TimesNewRomanBase = BaseFont.CreateFont(t, BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
             t = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Fonts), "timesbd.TTF");
             TimesNewRomanBaseBold = BaseFont.CreateFont(t, BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
-
-
-            //string ttf = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Fonts), "ARIAL.TTF");
-            //var baseFont = BaseFont.CreateFont(ttf, BaseFont.IDENTITY_H, BaseFont.NOT_EMBEDDED);
-            
-
-
             //-------------------------------------------------------------------------------------------------------------
 
 
 
             var pgSize = new iTextSharp.text.Rectangle(wi, he);
 
-
             document = new Document(pgSize, 0f, 0f, 0f, 0f);
 
-            
-          
-           
+
+
+
             //PdfReader reader;
 
             using (PdfWriter writer = PdfWriter.GetInstance(document, new FileStream(PDFName, FileMode.Create)))
@@ -142,27 +196,19 @@ namespace EtPDF
                 cb = writer.DirectContent;
 
                 addRoundRectangle(1, 1, 148, 26, 0.5, 3);
-                addEAN13("2520439000412", 2, 2, 30, 20);
+                addEAN13(barcode, 2, 2, 30, 15);
+                addText("Привет", 2, 2, 40, 10);
+                
                 document.NewPage();
                 addRoundRectangle(1, 1, 148, 26, 0.5, 3);
-                addEAN13("2520439000412", 2, 2, 30, 20);
-                document.NewPage();
-                addRoundRectangle(1, 1, 148, 26, 0.5, 3);
-                addEAN13("2520439000412", 2, 2, 30, 20);
-                document.NewPage();
-                addRoundRectangle(1, 1, 148, 26, 0.5, 3);
-                addEAN13("2520439000412", 2, 2, 30, 20);
-                document.NewPage();
-                addRoundRectangle(1, 1, 148, 26, 0.5, 3);
-                addEAN13("2520439000412", 2, 2, 30, 20);
-                document.NewPage();
-                addRoundRectangle(1, 1, 148, 26, 0.5, 3);
-                addEAN13("2520439000412", 2, 2, 30, 20);
-                document.NewPage();
-                addRoundRectangle(1, 1, 148, 26, 0.5, 3);
-                addEAN13("2520439000412", 2, 2, 30, 20);
-               
+                addEAN13(barcode, 2, 2, 30, 15);
+                addText("Hello", 2, 2, 40, 10);
 
+
+                document.NewPage();
+                addRoundRectangle(1, 1, 148, 26, 0.5, 3);
+                addEAN13(barcode, 2, 2, 30, 15);
+                addText("Hello", 2, 2, 40, 10);
 
                 //addWrapText("ИП Стеклянников В.М., Юр.адрес: Россия, 440045 г.Пенза, ул.Ладожская 155 - 77.Производство: Россия, 440015, г.Пенза, ул.Аустрина, д. 164.Хранить в крытых"
                 //    + "помещениях при температуре не ниже 10°С и отн. влажности воздуха от 40% до 60%. Гарант. срок эксплуатации-3 года. Срок службы-15 лет.ГОСТ 16371-2014,ГОСТ 19917-2014"
@@ -191,25 +237,21 @@ namespace EtPDF
                 ////addBorderText("ФАСАД ВЕРХ", 5, 130, 200, 100, 50f, true);
 
 
-
-
-
                 //addEAN13("2520439000412", 10,50, 1.5f);
 
                 document.Close();
                 writer.Close();
 
             }
+            // принтер -- имя бумаги -- ширина бумаги -- высота бумаги -- имя файла -- количество копий
+            //PrintPDF("NPIAE4A8A (HP LaserJet M402n)", "A4", 28, 150, PDFName, 1);
+            //PrintPDF("Microsoft Print to PDF", "A4", 28, 150, PDFName, 1);
 
-            PrintPDF("NPIAE4A8A (HP LaserJet M402n)", 28, 150, PDFName, 1);
 
 
-            Console.WriteLine(PageSize.A4.Width);
-            Console.WriteLine(PageSize.A4.Height);
-            Console.WriteLine(mm(210));
-            Console.ReadLine();
+           //Console.ReadLine();
 
-           
+
 
         }
 
@@ -241,11 +283,11 @@ namespace EtPDF
 
         static void addRoundRectangle(int x, int y, int w, int h, double border, double radius)
         {
-            cb.SetLineWidth(border/24.5*72);        
+            cb.SetLineWidth(border / 24.5 * 72);
             cb.RoundRectangle(mm(x), mm(y), mm(w), mm(h), mm(radius));
             cb.Stroke();
         }
-        static void addText(string text, int x, int y, float w, float fontSize, int ug=0 )
+        static void addText(string text, int x, int y, float w, float fontSize, int ug = 0)
 
         {
             float ww = TimesNewRomanBase.GetWidthPoint(text, fontSize);
@@ -264,7 +306,7 @@ namespace EtPDF
             cb.BeginText();
 
             if (bold)
-            { 
+            {
                 cb.SetFontAndSize(TimesNewRomanBaseBold, mm(fontSize));
                 if (ww > (w - wmargin * 5)) cb.SetHorizontalScaling((w - mm(wmargin * 5)) / ww * 100);
             }
@@ -274,24 +316,24 @@ namespace EtPDF
                 if (ww > (w - wmargin)) cb.SetHorizontalScaling((w - mm(wmargin)) / ww * 100);
             }
 
-            
-            
-            cb.ShowTextAligned(Element.ALIGN_CENTER, text, mm(x) + mm(w)/2, mm(y) + mm(h/2) - mm(fontSize/3), ug);
+
+
+            cb.ShowTextAligned(Element.ALIGN_CENTER, text, mm(x) + mm(w) / 2, mm(y) + mm(h / 2) - mm(fontSize / 3), ug);
             cb.EndText();
 
             addRectangle(x, y, w, h, 1);
 
 
-            cb.SetHorizontalScaling( 100 );
+            cb.SetHorizontalScaling(100);
 
         }
 
-        static void addWrapText(string text, int x, int y, int w, int h, float f) 
+        static void addWrapText(string text, int x, int y, int w, int h, float f)
         {
 
             ColumnText ct = new ColumnText(cb);
             ct.SetSimpleColumn(new Rectangle(mm(x), mm(y), mm(x + w), mm(y + h)));
-            
+
             Font font = new iTextSharp.text.Font(TimesNewRomanBase, f, Font.NORMAL);
 
             Paragraph tp = new Paragraph(text, font);
@@ -300,7 +342,7 @@ namespace EtPDF
             ct.Go();
         }
 
-        static void addEAN13(string cod, int x, int y, float k=1) 
+        static void addEAN13(string cod, int x, int y, float k = 1)
         {
             BarcodeEAN ean = new BarcodeEAN
             {
@@ -346,14 +388,14 @@ namespace EtPDF
 
         static void consPrinterName()
         {
-             PrinterSettings printer = new PrinterSettings();
-             foreach (var item in PrinterSettings.InstalledPrinters)
-             {
+            PrinterSettings printer = new PrinterSettings();
+            foreach (var item in PrinterSettings.InstalledPrinters)
+            {
                 Console.WriteLine(item);
                 Console.WriteLine("------------------");
                 //consPaperName(item.ToString());
                 //Console.WriteLine();
-             }
+            }
         }
 
 
@@ -362,7 +404,7 @@ namespace EtPDF
 
             var printerSettings = new PrinterSettings
             {
-                PrinterName = printer              
+                PrinterName = printer
             };
 
             foreach (PaperSize paperSize in printerSettings.PaperSizes)
